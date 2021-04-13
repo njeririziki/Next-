1282c633d300228150f562b9de2acb047fd2fd1d
Njeririziki <rizikinjeri@gmail.com>
2021-04-13 10:18:29 +0300

    Fix: about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,25 +1,27 @@
-import React from 'react'
-import Success from '../../public/successful_purchase.svg'
-import Computer from '../../public/Computer.svg'
-import Laptop from '../../public/Laptop.svg'
-import Man from '../../public/man_lift.svg'
-import Girl from '../../public/girl_laptop.svg'
+import React from 'react';
+import {Card,Typography,Space} from 'antd'
+// import Success from '../../public/successful_purchase.svg'
+// import Computer from '../../public/Computer.svg'
+// import Laptop from '../../public/Laptop.svg'
+// import Man from '../../public/man_lift.svg'
+// import Girl from '../../public/girl_laptop.svg'
+import Analytics from '../../public/Analytics.svg'
+import styles from '../../styles/About.modules.css'
 
 function AboutPage() {
     return (
-        <div style={{backgroundColor:'#efefef'}}>
-            <Laptop  style={{width:'200px', height:'200px'}}/>
-          <p>
-              Success full shoping spree
-
-            </p>  
-            <Computer style={{width:'200px', height:'200px'}}/>
-            <Man />
-            <Success style={{width:'500px', height:'400px'}}/>
-            <Girl />
+        <Space className={styles.root} >
+          < Card className={styles.fcard}>
+         <Typography.Title level={1}>
+         Success full shoping spree
+         </Typography.Title>
            
-            
-        </div>
+          </Card>
+          <Card className={styles.scard}>
+          <Analytics className={styles.svg} />
+          </Card>
+                
+        </Space>
     )
 }
 
